5620c92a4887883b1e899cbaae8d5e286439e29f
Banh Thien <banhlevuthien@gmail.com>
2015-05-11 15:29:57 +0700

    add define file, custom cel view

---
+++ App_Net_Client/define.h
@@ -0,0 +1,13 @@
+//
+//  define.h
+//  App_Net_Client
+//
+//  Created by iOSx New on 5/11/15.
+//  Copyright (c) 2015 BanhThien. All rights reserved.
+//
+
+#ifndef App_Net_Client_define_h
+#define App_Net_Client_define_h
+
+
+#endif
